b10cd95134a80a26098277d79343ee5677918297
Administrator <Administrator@PC201403051429>
2014-03-12 21:54:25 +0800

    1231231

--- src/ddd/sss11.java
+++ src/ddd/sss11.java
@@ -5,4 +5,9 @@ public class sss11 {
 	public static void main(String[] args) {
 		System.out.println("哈哈哈哈哈11111111111");
 	}
+	
+	
+	public void test1(){
+		System.out.println("!@#$%^&*()_");
+	}
 }
