eed07c8621db27420ecd64d033ef4fecda69c8b8
shenhua871231 <1051471778@qq.com>
2014-03-12 21:53:55 +0800

    Update sss11.java

--- src/ddd/sss11.java
+++ src/ddd/sss11.java
@@ -4,5 +4,7 @@ public class sss11 {
 
 	public static void main(String[] args) {
 		System.out.println("哈哈哈哈哈11111111111");
+		System.out.println("哈哈哈哈哈11111111111");
+		System.out.println("哈哈哈哈哈11111111111");
 	}
 }
