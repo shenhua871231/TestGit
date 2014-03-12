94522eb1ce8772ab65bd7a6307e61ba5cc424d88
Administrator <Administrator@PC201403051429>
2014-03-12 21:38:05 +0800

    77777

--- src/ddd/sss11.java
+++ src/ddd/sss11.java
@@ -3,6 +3,6 @@ package ddd;
 public class sss11 {
 
 	public static void main(String[] args) {
-		System.out.println("s1111屌33333丝男士111ss6666666s");
+		System.out.println("s1111屌33333丝男士111sss");
 	}
 }
